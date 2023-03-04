8eaee9939cbc19478d5989599c5a15a3597f75e7
aynurabdullayeva <126646069+aynurabdullayeva@users.noreply.github.com>
2023-03-05 00:03:25 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+//task1
+//const number = 56;
+//const firstNumber = number - 51;
+//const secondNumber = number - 50;
+
+//console.log(firstNumber, " ", secondNumber)
+
+//task2
+//const number2 = 123;
+//const firstReminder = number2 % 100;
+//const firstDigit = (number2 - firstReminder)/ 100;
+//const secondDigit = (number2 - 120);
+//console.log(firstDigit, " ", secondDigit)
+
+//task3
+//let student = 20;
+//let apple = 40;
+//console.log(apple / student)
+
+//task4
+//let student = 20;
+//let apple = 47;
+//console.log(apple % student)
+
+//task5
+//let stone = 5;
+//let stepan = 1;
+//friends = stone - stepan;
+//console.log(friends)
+
+//task6
+//let a = 7
+//let b = 9
+//perimeter = (a + b ) * 2;
+//console.log(perimeter)
+
+//task7
+//let a = 7;
+//let b = 9;
+//area =(a * b);
+//console.log(area)
+
+//task8
+//let a = 28;
+//console.log("a :", ++a)
+
+//task9
+//let a = 28
+//console.log("a :", --a)
+
+//task10
+//let Number = 15;
+//let oppositeNumber = -15;
+//oppositeNumber = ( Number * -1)
+//console.log(oppositeNumber)
+
+//task11
+//let firstNumber = 11;
+//let thirdNumber = 13;
+//secondNumber = ( firstNumber + thirdNumber) / 2;
+//console.log(secondNumber)
+
+//task12
+//const singerNumbers= 7;
+//const dancerNumbers= 16;
+//const bothOfThem = 4;
+//membersNumbers =(singerNumbers + dancerNumbers)-bothOfThem;
+//console.log(membersNumbers)
