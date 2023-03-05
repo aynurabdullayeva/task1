d39371a935b54d910c71567a3afbbb4b9159124a
aynurabdullayeva <126646069+aynurabdullayeva@users.noreply.github.com>
2023-03-05 20:41:33 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,68 +1,26 @@
 //task1
-//const number = 56;
-//const firstNumber = number - 51;
-//const secondNumber = number - 50;
-
-//console.log(firstNumber, " ", secondNumber)
-
-//task2
-//const number2 = 123;
-//const firstReminder = number2 % 100;
-//const firstDigit = (number2 - firstReminder)/ 100;
-//const secondDigit = (number2 - 120);
-//console.log(firstDigit, " ", secondDigit)
-
-//task3
-//let student = 20;
-//let apple = 40;
-//console.log(apple / student)
-
-//task4
-//let student = 20;
-//let apple = 47;
-//console.log(apple % student)
-
-//task5
-//let stone = 5;
-//let stepan = 1;
-//friends = stone - stepan;
-//console.log(friends)
+//const number = 16;
+//if(number >= 1) { 
+//console.log("Positive");
+//} else if(number == 0) {
+//console.log(Neither, Positive, nor ,Negative);
+//} else{
+//console.log("Negative")
+//}
 
 //task6
-//let a = 7
-//let b = 9
-//perimeter = (a + b ) * 2;
-//console.log(perimeter)
+//const n = 25;
+//if(n % 2 == 0) {
+//console.log("Even");
+//} else { 
+//console.log("Odd");
+//}
 
 //task7
-//let a = 7;
-//let b = 9;
-//area =(a * b);
-//console.log(area)
+//const number = 18;
+//const a = 9;
+//const b = 6;
+//const message = number % a == 0 ? "Yes": "No";
+//console.log(message)
 
-//task8
-//let a = 28;
-//console.log("a :", ++a)
 
-//task9
-//let a = 28
-//console.log("a :", --a)
-
-//task10
-//let Number = 15;
-//let oppositeNumber = -15;
-//oppositeNumber = ( Number * -1)
-//console.log(oppositeNumber)
-
-//task11
-//let firstNumber = 11;
-//let thirdNumber = 13;
-//secondNumber = ( firstNumber + thirdNumber) / 2;
-//console.log(secondNumber)
-
-//task12
-//const singerNumbers= 7;
-//const dancerNumbers= 16;
-//const bothOfThem = 4;
-//membersNumbers =(singerNumbers + dancerNumbers)-bothOfThem;
-//console.log(membersNumbers)
